a726a5a0ff75962fb85287561cfdd9f209468638
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2016-03-15 00:32:08 +0900

    オートモードを追加

--- src/Piano.ino
+++ src/Piano.ino
@@ -1,4 +1,5 @@
 #include "pitches.h"
+#include "music01.h"
 
 const int keyNums = 8;
 
@@ -29,33 +30,101 @@ const int speakerPins[keyNums] = {
 };
 
 const int notes[keyNums] = {
-  NOTE_A4,
-  NOTE_B4,
-  NOTE_C5,
-  NOTE_D5,
-  NOTE_E5,
-  NOTE_F5,
-  NOTE_G5,
-  NOTE_A5
+  NOTE_A5,
+  NOTE_B5,
+  NOTE_C6,
+  NOTE_D6,
+  NOTE_E6,
+  NOTE_F6,
+  NOTE_G6,
+  NOTE_A6
 };
 
-void setup() {
-  Serial.begin(9600);
+// trueの場合はrunManualMode関数を実行
+// falseの場合はrunAutoMode関数を実行
+bool isManualMode = true;
+int manualModeLimit = 400;
 
+void noToneAll() {
   for (int i = 0; i < keyNums; i++) {
-    pinMode(keyPins[i], INPUT_PULLUP);
+    noTone(speakerPins[i]);
   }
 }
 
-void loop() {
+
+// 32分音符のミリ秒
+const long tickMillis = 64;
+
+// 32分音符としての単位
+const int note32 = 32;
+int count = 0;
+int musicIndex = 0;
+void runAutoMode() {
+  if (count <= 0) {
+    noToneAll();
+    delay(tickMillis / 4); // 切れ目がほしい
+    musicIndex = musicIndex % (sizeof(music01) / sizeof(music01[0]));
+    int keyIndex = music01[musicIndex][0];
+    count = note32 / music01[musicIndex][1];
+    if (keyIndex >= 0 && keyIndex < keyNums) {
+      tone(speakerPins[keyIndex], notes[keyIndex]);
+    }
+    musicIndex++;
+  } else {
+    delay(tickMillis / 4);
+    count--;
+  }
+
+  // 32分音符を最小単位とする
+  delay(tickMillis / 4 * 3);
+
+  // スイッチがどれか押されていたらオートモード終了
   for (int i = 0; i < keyNums; i++) {
     int value = digitalRead(keyPins[i]);
     if (value == LOW) {
+      isManualMode = true;
+      manualModeLimit = 400;
+      noToneAll();
+      break;
+    }
+  }
+}
+
+void runManualMode() {
+  for (int i = 0; i < keyNums; i++) {
+    int value = digitalRead(keyPins[i]);
+    if (value == LOW) {
+      manualModeLimit = 400;
       tone(speakerPins[i], notes[i]);
     } else {
       noTone(speakerPins[i]);
     }
   }
   delay(50);
+
+  manualModeLimit--;
+  if (manualModeLimit < 0) {
+    isManualMode = false;
+    count = 0;
+    musicIndex = 0;
+  }
 }
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < keyNums; i++) {
+    pinMode(keyPins[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  if (isManualMode) {
+    runManualMode();
+  } else {
+    runAutoMode();
+  }
+}
+
+
+
 
